2e5eb139a9c9c8f4203412a7d8ca4d8cd6d16aac
Mayankyadav3980 <ymayank501@gmail.com>
2024-05-28 22:01:01 +0530

    sm change

---
+++ index.js
@@ -0,0 +1,12 @@
+var arr = []
+
+arr.push("hello")
+arr.push("world");
+arr.hello = 0
+console.log(arr.hello);
+// var value = 0
+// for(var i = 0;i < 5; i++) {
+//     value = arr[value]
+//     console.log(value);
+// }
+// console.log(value)
