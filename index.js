3ee3ba9465f487394275d938515661c29549cd5d
Mayankyadav3980 <ymayank501@gmail.com>
2024-06-01 16:38:26 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 let str = 'hello';
-console.log(str.length);
+str='Tello';
+let strN= str.replace('T','H');
+console.log(strN.length());
+// for(let c in str);
+
+// let ar= ['a', 'b'];
+// console.log(ar);
+// ar[0] = 'b';
+// console.log(ar);
+// ar= [1,2];
+// console.log(ar);
