24c42a8e773f31504cb5aadba905d382bce7f62c
Mayankyadav3980 <ymayank501@gmail.com>
2024-06-06 21:30:15 +0530

    updates

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-let str = 'hello';
-str='Tello';
-let strN= str.replace('T','H');
-console.log(strN.length());
-// for(let c in str);
-
-// let ar= ['a', 'b'];
-// console.log(ar);
-// ar[0] = 'b';
-// console.log(ar);
-// ar= [1,2];
-// console.log(ar);
+const a;
+// a=2;
+console.log(a);
