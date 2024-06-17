a33f2ad37ac13f119db549b734697ba573f4af68
Mayankyadav3980 <ymayank501@gmail.com>
2024-06-17 18:42:41 +0530

    iifis

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-function makeArmy() {
-    let shooters = [];
-    let i = 0;
-    while (i < 10) {
-      let shooter = function() {
-        console.log( i ); 
-      };
-      shooters.push(shooter);
-      i++;
-    }
-    return shooters;
-  }
-  let army = makeArmy();
-  army[0]();
-  army[5]();
-  army[1]();
+// var add = (function ( ) {
+//   var counter = 0;
+//   return function ( ) {
+//        counter += 1; 
+//        return counter;
+// }
+// })();    
+// console.log(add());
+// console.log(add());
+// console.log(add());
+
+
+let fun = (function(){
+  var counter = 0;
+  // console.log("this is iifi " + counter );
+  return function(){
+    counter++;
+    return counter;
+  };
+})();
+
+console.log(fun());
