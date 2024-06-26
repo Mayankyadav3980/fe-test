82828113adf52703aab7aa4bab51bb7464005306
Mayankyadav3980 <ymayank501@gmail.com>
2024-06-26 22:16:58 +0530

    usage of this

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-function Student(id, name, club){
-  this.id = id ;
-  this.name = name ;
-  this.club = club ;
- 
-}
-
-Student.prototype.printDetails = function(){
-      console.log(this.id, this.name, this.club);
-  }
-
-var s1 = new Student(1,'Raj', 'Cricket');
-var s2 = new Student(2,'Rohan','Dance');
-var s3 = new Student(3,'Rita','Basketball');
-var s4 = new Student(4,'Reema','Music');
-
-var arr=[s1, s2, s3, s4];
-
-for(let ob of arr){
-    ob.printDetails();
-}
+function bike() {
+  console.log(this);
+ }
+//  var name = "Ninja";
+ bike();
