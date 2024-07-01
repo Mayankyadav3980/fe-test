6324c0833126b540c41f049fdd5018c28f155cfd
Mayankyadav3980 <ymayank501@gmail.com>
2024-07-01 21:15:14 +0530

    scope check

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// x((a,b,c) => {
-//     return a+b+c
-//   },2,3,6)
-  
+// x( (a,b,c) => { return a+b+c }, 2,3,6 )
+//   var str = "";
+//   console.log(str);
 //   function x(callback,a,b,c) {
-//     let str = ""
+    
 //     setTimeout(() => {
 //       str += "x"
 //     });
@@ -12,13 +11,23 @@
 //     console.log(str)
 //   }
 
+let x = "x";
+console.log(x);
+function fun (){
+  console.log(x + 11);
+}
+fun();
 
-setTimeout(() => {
-  console.log('a')
-})
 
-new Promise((resolve,reject) => {
-  setTimeout(() => {
-    console.log('b')
-  })
-})
+// setTimeout(() => {
+//   console.log('a')
+// })
+
+// new Promise((resolve,reject) => {
+//   // setTimeout(() => {
+//   //   console.log('b')
+//   // })
+//   console.log('b');
+// })
+
+// console.log('c');
