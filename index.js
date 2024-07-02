00bcedaec698fe53e1cfc6a4259ae03f7aff9069
Mayankyadav3980 <ymayank501@gmail.com>
2024-07-02 20:41:16 +0530

    scope

--- index.js
+++ index.js
@@ -1,29 +1,5 @@
-const delay = (time) => {
-  // Write the logic here
-  return new Promise((res,rej) => {
-      setTimeout(res,time);
-  })
+let a = 1;
+{
+   var a= 1;
 }
- 
-let counter = () => {
-  let pause = false;
-  return{
-      start:  () => {
-        for(let i=0; ; i++){
-              if(pause)break;
-              console.log(i);
-              await delay(1000);
-          }
-      },
-
-       stop: () => {
-          pause = true;
-      }
-  }
-  
-}
-let count = counter();
-count.start()
-setTimeout(() => {
-  count.stop()
-},6000)
+console.log(a);
