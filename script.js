38d0d510bc81ddc676947bbdf415af0aaa9a41c1
Mayankyadav3980 <ymayank501@gmail.com>
2024-12-12 16:31:43 +0530

    understand double negation operator

---
+++ script.js
@@ -0,0 +1,21 @@
+// function a( mul ){
+
+//     // let x= 5;
+
+//     function b(x){
+//         let y= 6;
+//         // console.log(x);
+//         return x * mul;
+//     }
+//      return b;
+// }
+
+// let twoMul = a(2)
+// console.log(twoMul(4));
+
+console.log(!{n:'a'});
+console.log(!!{});
+
+// if(-1)console.log('inside if');
+
+
--- server.js
+++ server.js
@@ -1,5 +1,49 @@
-if('d'){
-    console.log('inside if');
-    
+let hr = document.getElementById("hr");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+let flag = 0;
+let h = 0, m = 0, s = 0;
+let arr = [];
+let id;
+function startWatch() {
+  
+  let t = 0;
+
+   id = setInterval(() => {
+    if (flag) {
+      
+    }
+    t = t + 1;
+    sec.innerHTML = `${t}`;
+
+    if (t == 60) {
+      m = m + 1;
+      t = 0;
+      min.innerHTML = m;
+    }
+    if (m == 60) {
+      h = h + 1;
+      m = 0;
+      hr.innerHTML = h;
+    }
+  }, 1000);
 }
-console.log('end of if');
+
+function pause(){
+    clearInterval(id);
+}
+function reset() {
+//   flag = true;
+    clearInterval(id);
+  sec.innerHTML = "0";
+  min.innerHTML = "0";
+  hr.innerHTML = "0";
+  m=0;
+  h=0;
+  s=0;
+}
+
+function split() {
+
+}
